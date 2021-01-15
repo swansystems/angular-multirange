e752761c7f91544bf667e9f8088c648958eb7832
David Wang <david@swansystems.com.au>
2021-01-15 10:38:43 +0800

    the angularjs dependency chnage to ^1.3.2

--- demo/controllers.js
+++ demo/controllers.js
@@ -5,18 +5,15 @@ angular.module('multirangeDemo')
 
     // populate some data
     $scope.rangeArray = [
-     { value: 0.20, name: 'Clock In',color:'red' },
-     { value: 0.40, name: 'Start Break',color:'green' },
-     { value: 0.66, name: 'End Break' ,color:'blue'},
-     { value: 0.80, name: 'Clock Out' ,color:'yellow' },
-	 { value: 0.90, name: 'Clock Out' ,color:'cyan' },
-	 { value: 0.50, name: 'Clock Out' ,color:'brown' }
+      { value: 0.20, name: 'Clock In', color: 'red' },
+      { value: 0.40, name: 'Start Break', color: 'green' },
+      { value: 0.50, name: 'Clock Out', color: 'brown' }
     ];
 
     $scope.views = vdsMultirangeViews.TIME;
 
     $scope.add = function () {
-      $scope.rangeArray.push({ value: parseFloat($scope.entry), name: 'Test entry',color:$scope.entryColor });
+      $scope.rangeArray.push({ value: parseFloat($scope.entry), name: 'Test entry', color: $scope.entryColor });
     };
 
   });
